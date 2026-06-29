09b0d71012824c3e21333b46148a0003005f2ed2
Tamboowneyi <tambowoneyi06@gmail.com>
2026-06-29 20:16:42 +0200

    Fixed a Typo

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedRoutes = [
+const allowedOrigins = [
   "http://localhost:5173",
   "https://flow-board-lemon.vercel.app",
 ];
